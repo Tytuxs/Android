478d8fbb56ea13498e39159aa1d53519a7a8054f
GerezAdrien <78793315+GerezAdrien@users.noreply.github.com>
2023-01-22 17:48:58 +0100

    MAJ validation mot de passe

--- app/src/main/java/com/example/android/Login.java
+++ app/src/main/java/com/example/android/Login.java
@@ -1,45 +1,63 @@
 package com.example.android;
 
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import org.w3c.dom.Text;
+        import android.content.Intent;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+        import android.widget.Toast;
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.os.Bundle;
+        import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
-    TextView username =(TextView) findViewById(R.id.editTextTextEmailAddress);
-    TextView password =(TextView) findViewById(R.id.editTextTextPassword);
-    Button buttonConnect = (Button) this.findViewById(R.id.loginButton);
+
+
+    private EditText mUsername;
+    private EditText mPassword;
+    private Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        buttonConnect.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
+        mUsername = findViewById(R.id.editTextTextEmailAddress);
+        mPassword = findViewById(R.id.editTextTextPassword);
+        mLoginButton = findViewById(R.id.loginButton);
 
-                    Log.i("TRACE", "COUCOU");
-                    if(username.getText().toString().equals("admin") && password.getText().toString().equals("Adri")){
-                        // Login success, start MainActivity
-                        Log.i("TRACE", "Connecté");
-                        Toast.makeText(Login.this,"VOUS ÊTES BIEN CONNECTÉ",Toast.LENGTH_SHORT).show();
-                        Intent mainIntent = new Intent(Login.this, MainActivity.class);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
 
-                        startActivity(mainIntent);
-                        finish();
-
-                    }else {
-                        // Login failed, display a message
-                        Log.i("TRACE", "NON-Connecté");
-                        Toast.makeText(Login.this,"LA CONNEXION A ÉCHOUÉE!!!",Toast.LENGTH_SHORT).show();
-                    }
+                if (isValidCredentials(username, password)) {
+                    Toast.makeText(Login.this,"VOUS ÊTES BIEN CONNECTÉ",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Login.this, MainMenuActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Login.this,"LA CONNEXION A ÉCHOUÉE!!!",Toast.LENGTH_SHORT).show();
                 }
+            }
         });
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        Log.i("TRACE", "COUCOU");
+        if(username.equals("admin") && password.equals("Adri")){
+
+            Log.i("TRACE", "Connecté");
+            /*Intent mainIntent = new Intent(Login.this, MainActivity.class);
+
+            startActivity(mainIntent);
+            finish();*/
+            return true;
+    }else {
+            Log.i("TRACE", "NON-Connecté");
+            Toast.makeText(Login.this,"LA CONNEXION A ÉCHOUÉE!!!",Toast.LENGTH_SHORT).show();
+            return false;
+        }
     }
 }
