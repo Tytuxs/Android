1d56e71035e04f94f0cdb6d9149fffd24f8aeba9
GerezAdrien <78793315+GerezAdrien@users.noreply.github.com>
2023-01-22 17:06:49 +0100

    MAJ Window

--- app/src/main/java/com/example/android/Login.java
+++ app/src/main/java/com/example/android/Login.java
@@ -1,6 +1,7 @@
 package com.example.android;
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,29 +13,33 @@ import org.w3c.dom.Text;
 public class Login extends AppCompatActivity {
     TextView username =(TextView) findViewById(R.id.editTextTextEmailAddress);
     TextView password =(TextView) findViewById(R.id.editTextTextPassword);
-    Button loginButton = (Button) this.findViewById(R.id.loginButton);
+    Button buttonConnect = (Button) this.findViewById(R.id.loginButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                // Login success, start MainActivity
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("Adri")){
+        buttonConnect.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
 
-                    Toast.makeText(Login.this,"VOUS ÊTES BIEN CONNECTÉ",Toast.LENGTH_SHORT).show();
-                    Intent mainIntent = new Intent(Login.this, MainActivity.class);
-                    startActivity(mainIntent);
-                    finish();
+                    Log.i("TRACE", "COUCOU");
+                    if(username.getText().toString().equals("admin") && password.getText().toString().equals("Adri")){
+                        // Login success, start MainActivity
+                        Log.i("TRACE", "Connecté");
+                        Toast.makeText(Login.this,"VOUS ÊTES BIEN CONNECTÉ",Toast.LENGTH_SHORT).show();
+                        Intent mainIntent = new Intent(Login.this, MainActivity.class);
 
-                }else {
-                    // Login failed, display a message
-                    Toast.makeText(Login.this,"LA CONNEXION A ÉCHOUÉE!!!",Toast.LENGTH_SHORT).show();
+                        startActivity(mainIntent);
+                        finish();
+
+                    }else {
+                        // Login failed, display a message
+                        Log.i("TRACE", "NON-Connecté");
+                        Toast.makeText(Login.this,"LA CONNEXION A ÉCHOUÉE!!!",Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
         });
     }
 }
